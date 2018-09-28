fd850fda63092689187903a31ee3d2bffc5f92d1
Matt <makeshifthidalgo@hotmail.com>
2018-09-27 21:12:29 -0700

    initial profile page

--- newApp/Screens/profileScreen.js
+++ newApp/Screens/profileScreen.js
@@ -1,30 +1,87 @@
 import React, { Component } from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
-
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  Image
+} from "react-native";
+import { Avatar, Card, Button, Icon } from "react-native-elements";
 
 class profileScreen extends Component {
-    state = {
-        isVisible: true,
-        user: "",
-        password: ""
-    };
-    render() {
-        return (
-          <ImageBackground source={require('../assets/beer-background.jpg')}
-            style={styles.container}>
-    
-           
-          </ImageBackground>
-        );
-      }
+  state = {};
+  render() {
+    return (
+      <ImageBackground
+        source={require("../assets/beer-background.jpg")}
+        style={styles.container}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            alignItems: "stretch",
+            justifyContent: "center",
+            marginTop: 50
+          }}
+        >
+          <Avatar
+            size="xlarge"
+            rounded
+            title="CR"
+            onPress={() => console.log("Works!")}
+            activeOpacity={0.7}
+          />
+        </View>
+        <ScrollView
+          style={{
+            flex: 1
+          }}
+        >
+          <View>
+            <Card title="CARD WITH DIVIDER">
+              {users.map((u, i) => {
+                return (
+                  <View key={i} style={styles.user}>
+                    <Image
+                      style={styles.image}
+                      resizeMode="cover"
+                      source={{ uri: u.avatar }}
+                    />
+                    <Text style={styles.name}>{u.name}</Text>
+                  </View>
+                );
+              })}
+            </Card>
+          </View>
+        </ScrollView>
+      </ImageBackground>
+    );
+  }
 }
 
 export default profileScreen;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      width: '100%',
-      height: '100%',
-    }, 
-  });
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%"
+  }
+});
+
+const users = [
+  {
+    name: "brynn",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg"
+  },
+  {
+    name: "brynn",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg"
+  },
+  {
+    name: "brynn",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg"
+  }
+];
