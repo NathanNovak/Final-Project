6f41e8ab78ed32a03bd25d9ac7e1bd2039bd5102
Nathan Novak <nnovak@ku.edu>
2018-09-29 10:08:45 -0700

    api and controller changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,48 @@
 const db = require("../models");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 module.exports = {
-    create: function (req, res) {
-        var salt = bcrypt.genSaltSync(10);
-        var hash = bcrypt.hashSync("Password", salt);
-        let user = {
-            email: "ncortes1415@gmail.com",
-            phone: "520-665-9464",
-            password: hash,
-            loggedIn: 'false'
+  create: function(req, res) {
+    console.log("CREATE");
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync("Password", salt);
+    let user = {
+      email: "ncortes1415@gmail.com",
+      phone: "520-665-9464",
+      password: hash,
+      loggedIn: "false"
+    };
+    db.Users.create({
+      firstName: "Bob",
+      lastName: "Smith",
+      email: "ncortes1415@gmail.com",
+      phone: "520-665-9464",
+      password: hash
+    }).then(function(dbModel) {
+      res.json(dbModel);
+      console.log("POSTED", dbModel);
+    });
+  },
 
-        }
-        db.User.create({
-            email: "ncortes1415@gmail.com",
-            phone: "520-665-9464",
-            password: hash
-        }).then(function (dbModel) {
-            res.json(dbModel);
-            console.log("POSTED", dbModel);
-        })
+  findAll: function(req, res) {
+    console.log("Get Saved");
+    db.Users.findAll({})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
 
-    }
+  findById: function(req, res) {
+    console.log(req.params);
+    db.Users.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
 
-}
+  delete: function(req, res) {
+    db.Users.destroy({
+      where: { id: req.params.id }
+    })
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+  }
+};
