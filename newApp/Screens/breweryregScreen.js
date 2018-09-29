e0786d6dd25136c2fd1380a039527fd908f54375
Kelby Behounek <knbehounek@hotmail.com>
2018-09-29 13:51:04 -0700

    brewREG

--- newApp/Screens/breweryregScreen.js
+++ newApp/Screens/breweryregScreen.js
@@ -6,27 +6,21 @@ import {
   StyleSheet,
   View,
   ImageBackground,
-  Alert,
-  ScrollView
+  Alert
 } from "react-native";
 import PasswordInputText from "react-native-hide-show-password-input";
-import { Input, Button, Overlay, FormLabel, FormInput, FormValidationMessage } from "react-native-elements";
+import { Input, Button, Overlay, Card } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
-class breweryregScreen extends Component {
+class loginScreen extends Component {
   state = {
     isVisible: true,
-    
-    password: "",
-    breweryname: "",
-    streetaddress: "",
-    emailaddress: "",
-    password: "",
-    phonenumber:"",
-    city:"",
-    st:"",
-    hours:"",
-    zipcode:"",
+    user: "",
+    password: ""
+  };
+
+  static navigationOptions = {
+    header: null
   };
 
   render() {
@@ -35,61 +29,78 @@ class breweryregScreen extends Component {
         source={require("../assets/beer-background.jpg")}
         style={styles.container}
       >
-        <View style={styles.overlayContainer}>
-          <View style={styles.top}>
-            <Text style={styles.header}>S P E N T G R A I N S</Text>
-          </View>
-          <Overlay isVisible={this.state.isVisible}>
-          <Input
+        <View>
+          <Text
+            style={styles.header}
+            containerStyle={{ backgroundColor: "#d3d3d3", opacity: 0.7 }}
+          >
+            S P E N T G R A I N S
+          </Text>
+
+          <Card
+            style={styles.overlayContainer}
+            containerStyle={{ backgroundColor: "#d3d3d3", opacity: 0.7 }}
+            isVisible={this.state.isVisible}
+          >
+            <Input
               placeholder="Enter Brewery Name"
+              placeholderTextColor="black"
               keyboardType="default"
               value={this.state.breweryname}
-              onChangeText={breweryname => this.setState({breweryname})}
+              onChangeText={breweryname => this.setState({ breweryname })}
             />
             <Input
               placeholder="Enter Street Address"
+              placeholderTextColor="black"
               keyboardType="default"
               value={this.state.streetaddress}
-              onChangeText={streetaddress => this.setState({streetaddress})}
+              onChangeText={streetaddress => this.setState({ streetaddress })}
             />
             <Input
               placeholder="Enter City"
+              placeholderTextColor="black"
               keyboardType="default"
               value={this.state.city}
-              onChangeText={city => this.setState({city})}
+              onChangeText={city => this.setState({ city })}
             />
             <Input
               placeholder="Enter State"
+              placeholderTextColor="black"
               keyboardType="default"
               value={this.state.st}
-              onChangeText={st => this.setState({st})}
+              onChangeText={st => this.setState({ st })}
             />
             <Input
               placeholder="Enter Zipcode"
+              placeholderTextColor="black"
               keyboardType="numeric"
               value={this.state.zipcode}
-              onChangeText={zipcode=> this.setState({zipcode})}
+              onChangeText={zipcode => this.setState({ zipcode })}
             />
             <Input
               placeholder="Enter Phonenumber"
+              placeholderTextColor="black"
               keyboardType="numbers-and-punctuation"
               value={this.state.phonenumber}
               onChangeText={phonenumber => this.setState({ phonenumber })}
             />
-             <Input
+            <Input
               placeholder="Enter Hours of Operation"
+              placeholderTextColor="black"
               keyboardType="numbers-and-punctuation"
               value={this.state.hours}
               onChangeText={hours => this.setState({ hours })}
             />
             <Input
               placeholder="Enter Email "
+              placeholderTextColor="black"
               keyboardType="email-address"
               value={this.state.emailaddress}
               onChangeText={emailaddress => this.setState({ emailaddress })}
             />
             <PasswordInputText
-              placeholder="Create Password"
+              placeholder="Password"
+              placeholderTextColor="black"
               value={this.state.password}
               onChangeText={password => this.setState({ password })}
             />
@@ -103,14 +114,14 @@ class breweryregScreen extends Component {
                 margin: 5
               }}
             />
-          </Overlay>
+          </Card>
         </View>
       </ImageBackground>
     );
   }
 }
 
-export default breweryregScreen;
+export default loginScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -118,22 +129,27 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%"
   },
-  overlayContainer: {
-    flex: 1
-  },
-  top: {
-    height: "50%",
-    alignItems: "center",
-    justifyContent: "center"
-  },
   header: {
+    textAlign: "center",
     color: "black",
+    padding: 20,
     fontSize: 28,
     borderColor: "black",
     borderWidth: 2,
-    padding: 20,
-    paddingLeft: 40,
-    paddingRight: 40,
-    backgroundColor: "white"
-  }
+    height: "15%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "20%",
+    marginLeft: "10%",
+    marginRight: "10%",
+    marginBottom: "20%",
+    backgroundColor: "#d3d3d3"
+  },
+  overlayContainer: {
+    color: "black",
+    alignItems: "center",
+    height: "50%",
+    backgroundColor: "#d3d3d3"
+  },
+  inputStyle: {}
 });
