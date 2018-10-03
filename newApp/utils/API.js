b94ebefd91d749277416af0ecb088fc345e8d924
Nathan Novak <nnovak@ku.edu>
2018-10-02 21:18:32 -0700

    changes

--- newApp/utils/API.js
+++ newApp/utils/API.js
@@ -1,9 +1,25 @@
-import axios from 'axios';
+// import axios from 'axios';
+// import fetch from 'fetch';
+// import {start} from 'repl';
+
+const url = "https://calm-peak-17945.herokuapp.com"
 
 export default {
 
 	saveUser: function (user) {
 		console.log("User Saved", user);
-		return axios.post("/api/users", user)
+		
+			fetch(url + "/api/users",{
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(user)
+			})
+			.then(response => 
+			console.log("Fetch Response", response))
+			.catch(err => console.log("Error", err))
+		
 	}
 }
