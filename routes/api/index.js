bc3fd1223af31980afbc68e5d32c63f46700ab4d
Nathan Novak <nnovak@ku.edu>
2018-10-03 16:03:14 -0700

    deleting backend

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const appRoutes = require("./apiRoutes");
-
-// Article routes
-router.use("/brewers", appRoutes);
-router.use("/users", appRoutes);
-
-module.exports = router; 
