72dfdd711e99578f57627a63eeec768680628941
Matt <makeshifthidalgo@hotmail.com>
2018-10-06 11:16:06 -0700

    user reg css done

--- newApp/Screens/userregScreen.js
+++ newApp/Screens/userregScreen.js
@@ -12,15 +12,21 @@ import {
 import PasswordInputText from "react-native-hide-show-password-input";
 import { Input, Button, Overlay, Card } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
+import RF from "react-native-responsive-fontsize";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 class userregScreen extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
   state = {
     isVisible: true,
     fname: "",
     lname: "",
     emailaddress: "",
     password: "",
-    phonenumber: "",
+    phonenumber: ""
   };
 
   handleClick = () => {
@@ -32,8 +38,8 @@ class userregScreen extends Component {
       emailaddress: this.state.emailaddress,
       password: this.state.password,
       phonenumber: this.state.phonenumber
-    })
-  }
+    });
+  };
 
   render() {
     return (
@@ -41,59 +47,71 @@ class userregScreen extends Component {
         source={require("../assets/beer-background.jpg")}
         style={styles.container}
       >
-        <View style={styles.overlayContainer}>
-          <View style={styles.top}>
-            <Text style={styles.header}>S P E N T G R A I N S</Text>
-          </View>
-          <Card isVisible={this.state.isVisible} 
-          containerStyle={{ backgroundColor: "#d3d3d3", opacity: 0.7 }}>
-          <Input
-              placeholder="Enter First Name"
-              placeholderTextColor="black"
-              keyboardType="default"
-              value={this.state.fname}
-              onChangeText={fname => this.setState({fname})}
-            />
-            <Input
-              placeholder="Enter Last Name"
-              placeholderTextColor="black"
-              keyboardType="default"
-              value={this.state.lname}
-              onChangeText={lname => this.setState({lname})}
-            />
-          <Input
-              placeholder="Enter Phonenumber"
-              placeholderTextColor="black"
-              keyboardType="numbers-and-punctuation"
-              value={this.state.phonenumber}
-              onChangeText={phonenumber => this.setState({ phonenumber })}
-          />
-            <Input
-              placeholder="Enter Email "
-              placeholderTextColor="black"
-              keyboardType="email-address"
-              value={this.state.emailaddress}
-              onChangeText={emailaddress => this.setState({ emailaddress })}
-            />
-            <PasswordInputText
-              placeholder="Create Password"
-              placeholderTextColor="black"
-              value={this.state.password}
-              onChangeText={password => this.setState({ password })}
-            />
-            <Button
-              title="GO TO PROFILE"
-              // onPress={() => this.props.navigation.navigate("Profile")}
-              onPress = {this.handleClick}
-              buttonStyle={{
-                backgroundColor: "black",
-                borderRadius: 5,
-                width: 200,
-                alignSelf: "center",
-                margin: 5,
+        <View>
+          <Text
+            style={styles.header}
+            containerStyle={{ backgroundColor: "#d3d3d3", opacity: 0.7 }}
+          >
+            S P E N T G R A I N S
+          </Text>
+          <KeyboardAwareScrollView>
+            <Card
+              isVisible={this.state.isVisible}
+              containerStyle={{
+                flex: 3,
+                backgroundColor: "#d3d3d3",
+                opacity: 0.7,
+                marginBottom: 500
               }}
-            />
-          </Card>
+            >
+              <Input
+                placeholder="Enter First Name"
+                placeholderTextColor="black"
+                keyboardType="default"
+                value={this.state.fname}
+                onChangeText={fname => this.setState({ fname })}
+              />
+              <Input
+                placeholder="Enter Last Name"
+                placeholderTextColor="black"
+                keyboardType="default"
+                value={this.state.lname}
+                onChangeText={lname => this.setState({ lname })}
+              />
+              <Input
+                placeholder="Enter Phonenumber"
+                placeholderTextColor="black"
+                keyboardType="numbers-and-punctuation"
+                value={this.state.phonenumber}
+                onChangeText={phonenumber => this.setState({ phonenumber })}
+              />
+              <Input
+                placeholder="Enter Email "
+                placeholderTextColor="black"
+                keyboardType="email-address"
+                value={this.state.emailaddress}
+                onChangeText={emailaddress => this.setState({ emailaddress })}
+              />
+              <PasswordInputText
+                placeholder="Create Password"
+                placeholderTextColor="black"
+                value={this.state.password}
+                onChangeText={password => this.setState({ password })}
+              />
+              <Button
+                title="GO TO PROFILE"
+                // onPress={() => this.props.navigation.navigate("Profile")}
+                onPress={this.handleClick}
+                buttonStyle={{
+                  backgroundColor: "black",
+                  borderRadius: 5,
+                  width: 200,
+                  alignSelf: "center",
+                  margin: 5
+                }}
+              />
+            </Card>
+          </KeyboardAwareScrollView>
         </View>
       </ImageBackground>
     );
@@ -109,7 +127,7 @@ const styles = StyleSheet.create({
     height: "100%"
   },
   overlayContainer: {
-    flex: 1
+    flex: 2
   },
   top: {
     height: "50%",
@@ -117,13 +135,19 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   header: {
+    textAlign: "center",
     color: "black",
-    fontSize: 28,
+    padding: 20,
+    fontSize: RF(4),
     borderColor: "black",
     borderWidth: 2,
-    padding: 20,
-    paddingLeft: 40,
-    paddingRight: 40,
-    backgroundColor: "white"
+    height: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "20%",
+    marginLeft: "10%",
+    marginRight: "10%",
+    marginBottom: "20%",
+    backgroundColor: "#d3d3d3"
   }
 });
