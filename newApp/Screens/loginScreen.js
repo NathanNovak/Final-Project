131228b3f198f03e532ed66f81674189a9629945
Matt <makeshifthidalgo@hotmail.com>
2018-10-06 10:45:19 -0700

    login screen complete

--- newApp/Screens/loginScreen.js
+++ newApp/Screens/loginScreen.js
@@ -13,6 +13,7 @@ import PasswordInputText from "react-native-hide-show-password-input";
 import { Input, Button, Overlay, Card } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 import RF from "react-native-responsive-fontsize";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 class loginScreen extends Component {
   state = {
@@ -25,15 +26,13 @@ class loginScreen extends Component {
     header: null
   };
 
-handleClick = (event, user) => {
-  event.preventDefault();
- user = this.state
- console.log("TEST", user);
+  handleClick = (event, user) => {
+    event.preventDefault();
+    user = this.state;
+    console.log("TEST", user);
 
-API.authenticate(user)
-
-
-}
+    API.authenticate(user);
+  };
 
   render() {
     return (
@@ -48,66 +47,83 @@ API.authenticate(user)
           >
             S P E N T G R A I N S
           </Text>
+          <KeyboardAwareScrollView>
+            <Card
+              style={styles.overlayContainer}
+              containerStyle={{
+                backgroundColor: "#d3d3d3",
+                opacity: 0.7,
+                marginBottom: 400
+              }}
+              isVisible={this.state.isVisible}
+            >
+              <Input
+                style={styles.inputStyle}
+                placeholderTextColor="black"
+                placeholder="Email"
+                rightIcon={<Icon name="user" size={24} color="black" />}
+                keyboardType="email-address"
+                value={this.state.user}
+                onChangeText={user => this.setState({ user })}
+              />
+              <PasswordInputText
+                placeholder="Password"
+                placeholderTextColor="black"
+                value={this.state.password}
+                onChangeText={password => this.setState({ password })}
+              />
+              <Button
+                buttonStyle={{
+                  backgroundColor: "black",
+                  borderRadius: 5,
+                  width: 200,
+                  alignSelf: "center",
+                  margin: 5
+                }}
+                icon={<Icon name="user" size={30} color="white" />}
+                title="USER LOGIN"
+                onPress={() => this.props.navigation.navigate("Home")}
+                //onPress={this.handleClick}
+              />
+              <Button
+                buttonStyle={{
+                  backgroundColor: "black",
+                  borderRadius: 5,
+                  width: 200,
+                  alignSelf: "center",
+                  margin: 5
+                }}
+                title="REGISTER USER"
+                onPress={() => this.props.navigation.navigate("UserReg")}
+              />
+              <Button
+                buttonStyle={{
+                  backgroundColor: "black",
+                  borderRadius: 5,
+                  width: 200,
+                  alignSelf: "center",
+                  margin: 5
+                }}
+                icon={<Icon name="beer" size={30} color="white" />}
+                title="BREWER LOGIN"
+                onPress={() => this.props.navigation.navigate("Home")}
+                //onPress={this.handleClick}
+              />
 
-          <Card
-            style={styles.overlayContainer}
-            containerStyle={{ backgroundColor: "#d3d3d3", opacity: 0.7 }}
-            isVisible={this.state.isVisible}
-          >
-            <Input
-              style={styles.inputStyle}
-              placeholderTextColor="black"
-              placeholder="Email"
-              rightIcon={<Icon name="user" size={24} color="black" />}
-              keyboardType="email-address"
-              value={this.state.user}
-              onChangeText={user => this.setState({ user })}
-            />
-            <PasswordInputText
-              placeholder="Password"
-              placeholderTextColor="black"
-              value={this.state.password}
-              onChangeText={password => this.setState({ password })}
-            />
-        
-        
-            <Button
-              buttonStyle={{
-                backgroundColor: "black",
-                borderRadius: 5,
-                width: 200,
-                alignSelf: "center",
-                margin: 5,
-              }}
-              icon={<Icon name="sign-in" size={15} color="white" />}
-              title="LOG IN"
-              // onPress={() => this.props.navigation.navigate("Home")}
-              onPress= {this.handleClick}
-            />
-            <Button
-              buttonStyle={{
-                backgroundColor: "black",
-                borderRadius: 5,
-                width: 200,
-                alignSelf: "center",
-                margin: 5,
-              }}
-              title="REGISTER USER"
-              onPress={() => this.props.navigation.navigate("UserReg")}
-            />
-            <Button
-              buttonStyle={{
-                backgroundColor: "black",
-                borderRadius: 5,
-                width: 200,
-                alignSelf: "center",
-                margin: 5,
-                zIndex: 1
-              }}
-              title="REGISTER BREWERY"
-              onPress={() => this.props.navigation.navigate("BrewReg")}
-            />
-          </Card>
+              <Button
+                buttonStyle={{
+                  backgroundColor: "black",
+                  borderRadius: 5,
+                  width: 200,
+                  alignSelf: "center",
+                  margin: 5,
+                  zIndex: 1
+                }}
+                title="REGISTER BREWERY"
+                onPress={() => this.props.navigation.navigate("BrewReg")}
+              />
+            </Card>
+          </KeyboardAwareScrollView>
         </View>
       </ImageBackground>
     );
@@ -129,7 +145,7 @@ const styles = StyleSheet.create({
     fontSize: RF(4),
     borderColor: "black",
     borderWidth: 2,
-    height: "15%",
+    height: "10%",
     alignItems: "center",
     justifyContent: "center",
     marginTop: "20%",
@@ -142,6 +158,7 @@ const styles = StyleSheet.create({
     color: "black",
     alignItems: "center",
     height: "50%",
-    backgroundColor: "#d3d3d3"
+    backgroundColor: "#d3d3d3",
+    marginBottom: 300
   }
 });
