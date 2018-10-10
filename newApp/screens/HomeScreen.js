68807174ad40847c9cab36fa30bc2d5b84bd209a
Kelby Behounek <knbehounek@hotmail.com>
2018-10-09 19:38:51 -0700

    homefix

--- newApp/screens/HomeScreen.js
+++ newApp/screens/HomeScreen.js
@@ -1,48 +1,63 @@
 import React, { Component } from "react";
 import {
+  ScrollView,
   StyleSheet,
   View,
   ImageBackground,
-  Image,
   Text,
-  ScrollView,
+  Image,
+  Alert,
+  Dimensions,
+  TouchableWithoutFeedback,
   Modal,
-  TouchableOpacity,
-  Alert
+  TouchableOpacity
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import {
-  SearchBar,
-  Card,
-  ListItem,
-  Button,
-  Header
-} from "react-native-elements";
+import { Avatar, Card, Button, Header } from "react-native-elements";
+import ImageElement from "../components/ImageElement";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { createStackNavigator } from "react-navigation";
 
-class homeScreen extends Component {
+class profileScreen extends Component {
   state = {
-    search: "",
-    brewers: [
-      "Crooked Tooth",
-      "Pueblo Vida",
-      "Button Brew House",
-      "1055 Brewery and Sausage House",
-      "Dragoon"
-    ],
-    modalVisible: false
+    modalVisible: false,
+    modalImage: require("../assets/beer1.jpg"),
+    images: [
+      require("../assets/beer1.jpg"),
+      require("../assets/beer2.jpg"),
+      require("../assets/beer3.jpg"),
+      require("../assets/beer4.jpg"),
+      require("../assets/beer5.jpg"),
+      require("../assets/beer6.jpg")
+    ]
   };
 
   static navigationOptions = {
     header: null
   };
 
-  setModalVisible(visible) {
+  setModalVisible(visible, imageKey) {
+    this.setState({ modalImage: this.state.images[imageKey] });
     this.setState({ modalVisible: visible });
   }
 
+  getImage() {
+    return this.state.modalImage;
+  }
+
+
   render() {
+    let images = this.state.images.map((val, key) => {
+      return (
+        <TouchableWithoutFeedback
+          key={key}
+          onPress={() => this.setModalVisible(true, key)}
+        >
+          <View style={styles.imageWrap}>
+            <ImageElement imgsource={val} />
+          </View>
+        </TouchableWithoutFeedback>
+      );
+    });
     return (
       <ImageBackground
         source={require("../assets/beer-background.jpg")}
@@ -57,7 +72,9 @@ class homeScreen extends Component {
           }}
           leftComponent={
             <TouchableOpacity
-            onPress={() => this.props.navigation.navigate("Login")}>
+            onPress={() => this.props.navigation.navigate("Login")}
+            // onPress={console.log(this.props.screenProps)}
+            >
               <Icon name="close" size={30} color="black" />
             </TouchableOpacity>
           }
@@ -76,50 +93,45 @@ class homeScreen extends Component {
             </TouchableOpacity>
           }
         />
-        <KeyboardAwareScrollView keyboardDismissMode="on-drag" ref="scrollView">
-          <View />
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "stretch",
+            justifyContent: "center",
+            marginTop: 50
+          }}
+        >
+          <Avatar
+            size="xlarge"
+            rounded
+            title="CR"
+            onPress={() => console.log("Works!")}
+            activeOpacity={0.7}
+          />
+        </View>
+        <KeyboardAwareScrollView
+          keyboardDismissMode="on-drag"
+          ref="scrollView"
+          style={{
+            flex: 4
+          }}
+        >
           <Card
-            title="Brewery News"
-            titleStyle={{ fontSize: 20, color: "black" }}
-            containerStyle={{ backgroundColor: "#d3d3d3", opacity: 0.7 }}
+            title={`${this.props.screenProps.currentUser.firstName} ${this.props.screenProps.currentUser.lastName}`}
+            containerStyle={{
+              marginBottom: 5,
+              alignItems: "stretch",
+              justifyContent: "center",
+              marginLeft: 0,
+              marginRight: 0,
+              backgroundColor: "#d3d3d3",
+              opacity: 0.7
+            }}
+            titleStyle={{
+              color: "black"
+            }}
           >
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "stretch",
-                justifyContent: "center"
-              }}
-            >
-              <Card
-                title="Brewery A"
-                titleStyle={{
-                  color: "black"
-                }}
-                containerStyle={{
-                  width: 250,
-                  backgroundColor: "#d3d3d3",
-                  opacity: 0.7
-                }}
-              >
-                <View>
-                  <Text
-                    textStyle={{
-                      color: "black"
-                    }}
-                  >
-                    The Hefeweizen over a power drill drink ruminates, and a
-                    bullfrog brew near a Corona Extra gets stinking drunk;
-                    however, a seldom molten Sierra Nevada teaches a bud light.
-                    Furthermore, a Fraoch Heather Ale behind the jersey cow
-                    daydreams, and the miller light behind a milwakees best
-                    slyly is a big fan of a Pilsner living with a Heineken.
-                    Another Octoberfest of the monkey bite operates a small bar
-                    with the PBR. Indeed, the Strohs from a miller has a change
-                    of heart about the bud light toward the Sam Adams.
-                  </Text>
-                </View>
-              </Card>
-            </View>
+            <Text>Email: {this.props.screenProps.currentUser.email}</Text>
           </Card>
           <Card
             title="Saved Breweries"
@@ -127,7 +139,9 @@ class homeScreen extends Component {
             containerStyle={{
               backgroundColor: "#d3d3d3",
               opacity: 0.7,
-              marginBottom: 300
+              marginBottom: 60,
+              marginLeft: 0,
+              marginRight: 0,
             }}
           >
             <View>
@@ -180,13 +194,53 @@ class homeScreen extends Component {
               </View>
             </View>
           </Card>
+          <ScrollView
+            horizontal={true}
+            contentContainerStyle={{
+              flexDirection: "row",
+              alignItems: "stretch",
+              justifyContent: "center"
+            }}
+          >
+            <Modal
+              style={styles.modal}
+              animationType={"fade"}
+              transparent={true}
+              visible={this.state.modalVisible}
+              onRequestClose={() => {}}
+            >
+              <View style={styles.modal}>
+                <Text
+                  style={styles.text}
+                  onPress={() => {
+                    this.setModalVisible(false);
+                  }}
+                >
+                  Close
+                </Text>
+                <ImageElement imgsource={this.state.modalImage} />
+              </View>
+            </Modal>
+            {images}
+          </ScrollView>
+          <Button
+            title="TAKE A PHOTO"
+            onPress={() => this.props.navigation.navigate("Camera")}
+            buttonStyle={{
+              backgroundColor: "black",
+              borderRadius: 5,
+              width: 200,
+              alignSelf: "center",
+              margin: 5
+            }}
+          />
         </KeyboardAwareScrollView>
       </ImageBackground>
     );
   }
 }
 
-export default homeScreen;
+export default profileScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -194,17 +248,27 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%"
   },
-  buttons: {
+  images: {
+    flex: 1,
     flexDirection: "row",
-    alignItems: "stretch",
-    justifyContent: "center",
-    marginTop: 50
+    flexWrap: "wrap"
   },
-  button: {
-    backgroundColor: "blue",
-    borderRadius: 5,
-    padding: 20,
-    alignSelf: "center",
-    margin: 10
+  imageWrap: {
+    margin: 2,
+    padding: 2,
+    height: Dimensions.get("window").height / 3 - 12,
+    width: Dimensions.get("window").width / 2 - 4,
+    backgroundColor: "#d3d3d3"
+  },
+  modal: {
+    flex: 1,
+    padding: 40,
+    backgroundColor: "transparent"
+  },
+  text: {
+    color: "black"
+  },
+  header: {
+    backgroundColor: "#d3d3d3"
   }
 });
